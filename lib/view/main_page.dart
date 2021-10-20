fa590868994c4f168f1d71daa75cd24f61fae5f4
athallahmaajid <athallahmaajid@gmail.com>
2021-10-20 16:19:37 +0700

    fix everything :)

--- lib/view/main_page.dart
+++ lib/view/main_page.dart
@@ -14,7 +14,7 @@ class _MainPageState extends State<MainPage> {
   late PRLeaderboard prLeaderboard;
 
   Future<void> fetchPRLeaderboard() async {
-    final response = await http.get(Uri.parse("https://bellshade-server.herokuapp.com/PRLeaderboard/pr"));
+    final response = await http.get(Uri.parse("https://bellshade-server.herokuapp.com/leaderboard/pr"));
     prLeaderboard = PRLeaderboard.fromJson(jsonDecode(response.body));
   }
 
@@ -24,81 +24,67 @@ class _MainPageState extends State<MainPage> {
       backgroundColor: Theme.of(context).backgroundColor,
       appBar: AppBar(
         title: const Text("Bellshade PRLeaderboard"),
-        bottom: const TabBar(
-          tabs: [
-            Tab(
-              child: Text("PRs"),
-            ),
-            Tab(
-              child: Text("Commits"),
-            ),
-          ],
-        ),
       ),
-      body: TabBarView(
-        children: [
-          SingleChildScrollView(
-            child: FutureBuilder(
-              future: fetchPRLeaderboard(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  return DataTable(
-                    columnSpacing: 0,
-                    dataRowHeight: 56,
-                    columns: [
-                      DataColumn(
-                        label: Text("Rank", style: TextStyle(color: Theme.of(context).primaryColor)),
-                      ),
-                      DataColumn(
-                        label: Container(
-                            margin: const EdgeInsets.only(right: 10, left: 10),
-                            child: Text("Avatar", style: TextStyle(color: Theme.of(context).primaryColor))),
-                      ),
-                      DataColumn(
-                        label: Container(
-                            margin: EdgeInsets.only(right: MediaQuery.of(context).size.width * 0.4),
-                            child: Text("Username", style: TextStyle(color: Theme.of(context).primaryColor))),
-                      ),
-                      DataColumn(
-                        label: Text("PRs", style: TextStyle(color: Theme.of(context).primaryColor)),
-                      ),
-                    ],
-                    rows: prLeaderboard.users
-                        .map(
-                          (e) => DataRow(
-                            cells: [
-                              DataCell(
-                                Text((prLeaderboard.users.indexOf(e) + 1).toString(), style: TextStyle(color: Theme.of(context).primaryColor)),
-                              ),
-                              DataCell(
-                                Container(
-                                  margin: const EdgeInsets.only(bottom: 5, top: 5),
-                                  decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    image: DecorationImage(
-                                      image: NetworkImage(e.avatar),
-                                    ),
-                                  ),
+      body: SingleChildScrollView(
+        child: FutureBuilder(
+          future: fetchPRLeaderboard(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return DataTable(
+                columnSpacing: 0,
+                dataRowHeight: 56,
+                columns: [
+                  DataColumn(
+                    label: Text("Rank", style: TextStyle(color: Theme.of(context).primaryColor)),
+                  ),
+                  DataColumn(
+                    label: Container(
+                        margin: const EdgeInsets.only(right: 10, left: 10),
+                        child: Text("Avatar", style: TextStyle(color: Theme.of(context).primaryColor))),
+                  ),
+                  DataColumn(
+                    label: Container(
+                        margin: EdgeInsets.only(right: MediaQuery.of(context).size.width * 0.4),
+                        child: Text("Username", style: TextStyle(color: Theme.of(context).primaryColor))),
+                  ),
+                  DataColumn(
+                    label: Text("PRs", style: TextStyle(color: Theme.of(context).primaryColor)),
+                  ),
+                ],
+                rows: prLeaderboard.users
+                    .map(
+                      (e) => DataRow(
+                        cells: [
+                          DataCell(
+                            Text((prLeaderboard.users.indexOf(e) + 1).toString(), style: TextStyle(color: Theme.of(context).primaryColor)),
+                          ),
+                          DataCell(
+                            Container(
+                              margin: const EdgeInsets.only(bottom: 5, top: 5),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                  image: NetworkImage(e.avatar),
                                 ),
                               ),
-                              DataCell(
-                                Text(e.name, style: TextStyle(color: Theme.of(context).primaryColor)),
-                              ),
-                              DataCell(
-                                Text(e.prCount.toString(), style: TextStyle(color: Theme.of(context).primaryColor)),
-                              ),
-                            ],
+                            ),
                           ),
-                        )
-                        .toList(),
-                  );
-                } else {
-                  return const Center(child: CircularProgressIndicator());
-                }
-              },
-            ),
-          ),
-        ],
+                          DataCell(
+                            Text(e.name, style: TextStyle(color: Theme.of(context).primaryColor)),
+                          ),
+                          DataCell(
+                            Text(e.prCount.toString(), style: TextStyle(color: Theme.of(context).primaryColor)),
+                          ),
+                        ],
+                      ),
+                    )
+                    .toList(),
+              );
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
       ),
     );
   }
